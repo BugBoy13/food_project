ecc6ae1700433b93eecf87108db41405ac599d2d
vardaan <vardaan13121997@gmail.com>
2021-07-31 13:58:37 +0530

    Intro screen done

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const LoginScreen = () => {
+    return (
+        <Text>LoginScreen</Text>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default LoginScreen;
